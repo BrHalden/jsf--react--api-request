8e5f7eac863db5b1fbfe0816453cdf56b3f0fcbe
Kristoffer Bråthen Halden <>
2020-03-19 17:24:51 +0100

    Done

--- src/components/NewsArticleList.js
+++ src/components/NewsArticleList.js
@@ -1,9 +1,23 @@
-import React from 'react';
+// API: https://jsonplaceholder.typicode.com/posts
+import React, {useState, useEffect} from 'react';
 
 export default function NewsArticleList() {
-
+  const [state, setState] = useState({
+    articles: []
+  });
+  useEffect(function(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(articles => setState({
+        articles: articles
+      }));
+  }, [])
   return (
     <ul>
+      {
+      state.articles
+        .map(article => <li key={article.id}>{article.title}</li>)
+      } 
     </ul>
   );
 }
